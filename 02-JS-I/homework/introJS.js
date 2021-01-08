cfcda66d369bd5ce3bb11c6992cd14c86e9f0e88
SARAR16 <sararua_96@hotmail.com>
2021-01-07 20:54:58 -0500

    Corrijo archivo intro JS

---
+++ 02-JS-I/homework/introJS.js
@@ -0,0 +1,10 @@
+## Instrucciones
+---
+1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años
+Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+	* Variables: Una variable es el lugar que usamos dentro de un programa de computadora para guardar datos importantes que luego podemos usar.
+	* Strings: Es cualquier tipo de texto que se desee almacenar dentro de un programa de computadora, para que se pueda guardar lo vamos a encerrar entre comillas simples o dobles ("", '')
+	* Funciones (argumentos, `return`): Una función es un conjunto de instrucciones que le dicen al programa o sistema cómo se deben ejectutar.
+	* Declaraciones `if`: 
+	* Valores booleanos (`true`, `false`): Los valores booleanos son dos opciones de respuesta que nos ofrece un programa de computadora, puede ser true, para un valor verdadero o false, para un valor negativo o erroneo.
