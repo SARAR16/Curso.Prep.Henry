da3a6f3e003b7b3ee74e19e333281861d4f42433
SARAR16 <sararua_96@hotmail.com>
2021-01-10 21:48:20 -0500

    Agrego archivo sobre conceptos JS

---
+++ 03-JS-II/homework/conceptos.js
@@ -0,0 +1,6 @@
+1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+* `for`: Es una estructura que se utiliza para repetir un código las veces que se le sea asignado.
+* `&&`: (Y - AND) Es un operador lógico que se encarga de evaluar que dos expresiones se cumplan y sean verdaderas.
+* `||`: (O - OR) Es un operador lógico que evaluar que al menos una de dos o varias expresiones se cumpla o sea verdadera.
+* `!`: (NO - NOT) Es una operador lógico que niega o devuelve el valor opuesto a la expresion dada.
