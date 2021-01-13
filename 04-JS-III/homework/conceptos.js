b193c109fbab4ccb4ebb0cc5893d27fe0c133a20
SARAR16 <sararua_96@hotmail.com>
2021-01-13 17:03:48 -0500

    Agrego archivo conceptos

---
+++ 04-JS-III/homework/conceptos.js
@@ -0,0 +1,3 @@
+En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes conceptos como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a descubrir rápidamente cualquier agujero en tu comprensión.
+
+Arrays: Los arrays son objetos similares a una lista, que permiten guardar elementos del mismo tipo.
